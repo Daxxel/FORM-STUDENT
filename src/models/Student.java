cf05d8ef54abacf55c132362701e5b4c947189cb
Max Alejandro Flores Rodriguez <MaxAlejandro.FloresRodriguez@fuller.com.mx>
2025-04-23 12:30:48 -0600

    Add Student model class with basic attributes

---
+++ src/models/Student.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Student {
+    public int Id;
+    public String Name;
+    public String lastName;
+    public String secondLastName;
+    public String career;
+    public String semester;
+    public String email;
+}
