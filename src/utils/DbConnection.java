4bf75bcdd777fefbd8876090ac3b6798a05671c7
Max Alejandro <max.flores.ar@gmail.com>
2025-04-27 18:26:35 -0600

    Add DbConnection class for database connection management

---
+++ src/utils/DbConnection.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static final String URL = System.getenv("DB_HOST");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if(connection == null) {
+            connection = buildConnection();
+        }
+
+        return connection;
+    }
+
+    private static Connection buildConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, DB_USER, DB_PASSWORD);
+            
+            return connection;
+        } catch (SQLException exception) {
+            throw new RuntimeException("Error connecting to the database", exception);
+        }
+    }
+}
